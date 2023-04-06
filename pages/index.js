d32e0274628e19967ac2afc2ba051509fd49b766
sdscoelho86 <62630922+sdscoelho86@users.noreply.github.com>
2023-04-06 12:19:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>ClickBarco</title>
+        <title>ClickBarco APP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="ClickBarco" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Compre passagens de embarcações online!
         </p>
       </main>
 
